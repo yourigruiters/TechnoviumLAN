45bb288266bfc96685a9ff543b577688a645e994
Youri Gruiters <i-youri-i@hotmail.com>
2022-03-15 12:39:51 +0100

    Project klaargezet voor samenwerking

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-// const voornaam = "Youri";
-// const achternaam = "Gruiters";
-
-// const volledigeNaam =
-//   "Hallo, mijn naam is: " + voornaam + " " + achternaam + ".";
-// const volledigeNaam2 = `Hallo, mijn naam is: ${voornaam} ${achternaam}.`;
-
-// console.log(volledigeNaam);
-// console.log(volledigeNaam2);
-
 const button = document.querySelector("#klik");
 const container = document.querySelector("#cont");
 
