f91bebfd15cba664b14a9f0fbad2818adbe02402
Youri Gruiters <i-youri-i@hotmail.com>
2022-03-22 19:45:46 +0100

    added first instance of modal - needs updating

--- js/register.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector("#klik");
-const container = document.querySelector("#cont");
-
-button.addEventListener("click", () => {
-  container.style.top = "150px";
-});
