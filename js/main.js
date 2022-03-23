1ee5db5d7400457d6f32e30d6d25c865e7955176
Youri Gruiters <56152938+yourigruiters@users.noreply.github.com>
2022-03-23 09:22:23 +0100

    Layout setup implemented (#2)
    * Layout setup implemented
    
    * added first instance of modal - needs updating
    
    * test
    
    * test
    
    Co-authored-by: ThijmenJava <1194425@student.roc-nijmegen.nl>

--- js/main.js
+++ js/main.js
@@ -1,6 +1,77 @@
-const button = document.querySelector("#klik");
-const container = document.querySelector("#cont");
+const header = document.querySelector('header');
 
-button.addEventListener("click", () => {
-  container.style.top = "150px";
+// Sticky navigation
+$(window).ready(() => {
+    if (window.pageYOffset > $(window).height() - header.offsetHeight) {
+        header.classList.add('sticky');
+    }
 });
+
+$(window).scroll(() => {
+    if (window.pageYOffset > $(window).height() - header.offsetHeight) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
+
+// Register/Login overlay
+const aanmelden = document.querySelector('#aanmelden');
+const register = document.querySelector('#register');
+
+aanmelden.addEventListener('click', () => {
+    register.style.opacity = '1';
+    register.style.zIndex = '5000';
+
+    disableScroll();
+
+    // document.body.style.position = 'fixed';
+    // document.body.style.top = `-${window.scrollY}px`;
+}); // left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener(
+        'test',
+        null,
+        Object.defineProperty({}, 'passive', {
+            get: function () {
+                supportsPassive = true;
+            },
+        })
+    );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent =
+    'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
