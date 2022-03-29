a6224b3cf7231dce97b0224147dbdd863ad663dd
ThijmenJava <1194425@student.roc-nijmegen.nl>
2022-03-29 07:48:06 +0200

    test

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let countDownDate = new Date("Jan 5, 2024 15:00:00").getTime();
+
+let time = setInterval( () => {
+
+    let now = new Date().getTime();
+    let timeDistance = countDownDate - now;
+
+    let days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(timeDistance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(timeDistance % (1000 * 60 * 60) / (1000 * 60));
+    let secondes = Math.floor(timeDistance % (1000 * 60) / (1000));
+
+    const countdownTimer = document.querySelector("#countdown");
+
+    countdownTimer.innerHTML = days + "D " + hours + "H " + minutes + "M " + secondes + "S ";
+
+    if(timeDistance < 0) {
+        clearInterval(x);
+        countdownTimer.innerHTML = "EXPIRED";       
+    }
+
+}, 1000);
+
+
